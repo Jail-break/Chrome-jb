aa9d86ca49b4023b15801d310164f1603fbe6107
Jail-break <tobybass2003@gmail.com>
2016-09-08 12:17:28 -0400

    Update call.cpp

--- src/call.cpp
+++ src/call.cpp
@@ -4,23 +4,23 @@
 
 extern void pmterm(const char *);
 
-extern char * plterm();
+extern char * plterm(int a);
 
 void call(){
 	if(eax == 0 && rax != 0){
 		switch(rax){
 			case 1:
-				if(ebx != 0){
+				if(rbx != 0){
 					pmterm(strcat("exit$", std::to_string(ebx).c_str()));
 				}else{
 					pterm("exit$");
 				}
 				break;
 			case 3:
-				if(ebx == 1){
-					
+				if(rbx == 1){
+					plterm(6)
 				}else{
-					asm	
+					asm("int $0x80");
 				}
 				break;
 
